7b033299503318141b1e5767bf973e7ede0b21af
abdulaziz <jkg9rmd8td@privaterelay.appleid.com>
2026-03-02 16:40:29 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00ffd5",
+        darkbg: "#0b1117"
+      }
+    }
+  },
+  plugins: [],
+}
