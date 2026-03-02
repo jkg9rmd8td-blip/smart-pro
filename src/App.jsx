cb724592dc8064751fa39453fdf49ddaaede5ff7
abdulaziz <jkg9rmd8td@privaterelay.appleid.com>
2026-03-02 16:53:28 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
+import Players from "./pages/Players";
+import Executive from "./pages/Executive";
+import Reports from "./pages/Reports";
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/executive" element={<Executive />} />
+          <Route path="/reports" element={<Reports />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
